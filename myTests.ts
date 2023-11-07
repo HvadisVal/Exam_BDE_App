bb1d029f4877f9a05d6514739fad78f19b11f8ac
HvadisVal <114469812+HvadisVal@users.noreply.github.com>
2023-11-07 11:23:52 +0100

    test3

--- myTests.ts
+++ myTests.ts
@@ -25,4 +25,42 @@ test('Basic Arithmetic Operations', async t => {
 
 // Test 2 - Division by zero
 
+// Test 2
+test('Division by zero should show an error', async t => {
+    // Define selectors for buttons and result field
+    const fourButton = Selector('button').withText('4');
+    const divideButton = Selector('button').withText('/');
+    const zeroButton = Selector('button').withText('0');
+    const equalsButton = Selector('button').withText('=');
+    const resultField = Selector('#result');
 
+    // Perform division by zero
+    await t
+        .click(fourButton)
+        .click(divideButton)
+        .click(zeroButton)
+        .click(equalsButton);
+
+    // Check if the result is 'Infinity'
+    await t.expect(resultField.value).eql('Infinity');
+});
+
+// Test 3
+test('Multiplication of two numbers', async t => {
+    // Define selectors for buttons and result field
+    const threeButton = Selector('button').withText('3');
+    const multiplyButton = Selector('button').withText('*');
+    const fourButton = Selector('button').withText('4');
+    const equalsButton = Selector('button').withText('=');
+    const resultField = Selector('#result');
+
+    // Perform multiplication
+    await t
+        .click(threeButton)
+        .click(multiplyButton)
+        .click(fourButton)
+        .click(equalsButton);
+
+    // Check if the result is '12'
+    await t.expect(resultField.value).eql('12');
+});
